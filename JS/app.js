5ac208ca0e765de9ede5bce8a149de3f552a312f
Azur-azabache <tharuka_20@hotmail.com>
2017-11-09 18:29:33 -0500

    subo archivo

---
+++ JS/app.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var board= document.querySelector('.board-js');
+    
+    board.addEventListener('click',addX);
+      
+    };
+ 
+ var centinel = true;
+    
+ function addX(event){
+    if(event.target.matches('td') && event.target.textContent===''){
+    if(centinel)
+     event.target.textContent = 'X';
+    else
+     event.target.textContent ='O';
+ 
+     centinel=!centinel;
+    }
+ }
+
+ //Declaro mi evento
+ document.querySelector.addEventListener('click',clear);
+
+ //Declaro mi función
+ 
